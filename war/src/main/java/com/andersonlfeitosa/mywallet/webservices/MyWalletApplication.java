07d19065b20a1e4f4e6d7f835219d6a57e881eed
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-11-24 16:26:47 -0200

    commit inicial

---
+++ war/src/main/java/com/andersonlfeitosa/mywallet/webservices/MyWalletApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Desenvolvido por Anderson Lobo Feitosa, 2014
+ */
+package com.andersonlfeitosa.mywallet.webservices;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Ponto de entrada da API RESTFul.
+ *
+ * @author andersonlf@gmail.com
+ */
+@ApplicationPath("api")
+public class MyWalletApplication extends Application {
+	
+}
